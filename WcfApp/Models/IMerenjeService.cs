48ae3d83d64a58ed5ada049f705252483c675153
tinar24 <116080215+tinar24@users.noreply.github.com>
2022-12-29 23:55:40 +0100

    Update IMerenjeService.cs

--- WcfApp/Models/IMerenjeService.cs
+++ WcfApp/Models/IMerenjeService.cs
@@ -12,6 +12,64 @@ namespace Models
     {
         [OperationContract]
         ICollection<Models.Merenje> GetMerenjes();
+        
+        [OperationContract]
+        double GetAzuriranuVrednost(int id);
+
+        [OperationContract]
+        List<double?> GetAzuriraneVrednostiUredjaja();
+
+
+        [OperationContract]
+        List<Merenje> GetAnalogni();
+
+        
+        [OperationContract]
+        List<Merenje> GetDigitalni();
+    }
+    
+    [ServiceContract]
+    public interface IServerMerenjeService
+    {
+        /// <summary>
+        /// Vraca sve id-ijeve iz baze, da bi se ti objekti mogli pronaci
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>IdDb -> Timestamp</returns>
+        [OperationContract]
+        IDictionary<int, long> GetAllTimestampsById(int id);
+
+        [OperationContract]
+        long GetLastTimestampById(int id);
+
+        [OperationContract]
+        Merenje GetLastMerenjeFromIdMerenje(int id);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>IdDevice -> Timestamp</returns>
+        [OperationContract]
+        IDictionary<long, long> GetTimestampPerDevice();
+
+        [OperationContract]
+        IDictionary<long, long> GetTimestampsAnalog();
+
+        [OperationContract]
+        IDictionary<long, long> GetTimestampsDigital();
+
+        [OperationContract]
+        Models.Merenje GetMerenjeByDbId(int dbId);
+
+
+        [OperationContract]
+        Merenje GetLastForDeviceId(int id);
+
     }
 
+    [ServiceContract]
+    public interface IWrite
+    {
+        [OperationContract]
+        void WriteDevice(Models.Merenje merenje);
+    }
 }
