30a24456706ea083a7029f7cb8c678b6d032b68c
duki72 <duskadamjanovic493@gmail.com>
2023-01-05 23:32:03 +0100

    ProxyService

---
+++ WcfApp/Proxy/ProxyService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using static Server.Conversion;
+
+namespace Server
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class MerenjeService : Models.IServerMerenjeService, Models.IWrite
+    {
+        private Conversion conversion = new Conversion();
+        private DBCRUD.IDBCRUD crud { get; set; } = null;
+        public MerenjeService(DBCRUD.IDBCRUD crud) : base()
+        {
+            this.crud = crud;
+        }
+
+    }
+}
