dd2581f4970646cdc8c4c4ed387fcafeefbb92bc
duki72 <duskadamjanovic493@gmail.com>
2023-01-09 11:56:47 +0100

    ProxyService

--- WcfApp/Proxy/ProxyService.cs
+++ WcfApp/Proxy/ProxyService.cs
@@ -33,4 +33,37 @@ namespace Proxy
                 return LocalStorage;
             }
         }
+        public void SetLocalStorage(List<ProxyMerenje> merenja)
+        {
+            lock (LocalStorage)
+            {
+                LocalStorage = merenja;
+            }
+        }
+
+
+        [OperationBehavior]
+        public void CheckRemovals()
+        {
+            ProxyLogger.log.Info("Trying to remove instances that are older than 1 day in proxy service.");
+            lock (LocalStorage)
+            {
+                LocalStorage.RemoveAll(x => x.PoslednjiPutProcitano.CompareTo(DateTime.Now.AddDays(-1)) < 0);
+            }
+        }
+
+        public void UpdateLocalStorageWithNewMerenje(Models.Merenje merenje)
+        {
+            ProxyLogger.log.Info($"Proxy service updating local storage with new merenje:{merenje}");
+            lock (LocalStorage)
+            {
+                LocalStorage.RemoveAll(x => x.MerenjeInfo.IdDb == merenje.IdDb);
+                LocalStorage.Add(new ProxyMerenje()
+                {
+                    MerenjeInfo = merenje,
+                    PoslednjiPutDobavljeno = DateTime.Now,
+                    PoslednjiPutProcitano = new DateTime(merenje.Timestamp)
+                });
+            }
+        }
 
