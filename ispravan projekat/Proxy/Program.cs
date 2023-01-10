3ed4d7a642b083328c5b6048ab3fb0b317a5efd1
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2023-01-10 12:17:16 +0100

    Add files via upload

---
+++ ispravan projekat/Proxy/Program.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        [ExcludeFromCodeCoverage]
+
+        static void Main(string[] args)
+        {
+            var KonekcijaProxyAsClient = new Models.Konekcije.KlijentKonekcija<IServerMerenjeService>(
+                Models.Konekcije.Konekcija.UriServer
+            );
+
+            var ProxyService = new ProxyService(KonekcijaProxyAsClient.Service);
+
+            var KonekcijaProxyAsServer = new Models.Konekcije.ServerKonekcija<IMerenjeService>(
+                new string[] { Models.Konekcije.Konekcija.UriProxyServer }, ProxyService);
+
+            Console.WriteLine("Proxy se digao");
+            Proxy proxy = new Proxy(KonekcijaProxyAsServer, KonekcijaProxyAsClient, new ProxyInput());
+            proxy.StartProxy();
+            proxy.StartProxyServices();
+        }
+    }
+}
