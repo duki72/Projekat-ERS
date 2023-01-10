4e6f93c21416a822f4accfccb5f67f7b42a65cc6
duki72 <72766124+duki72@users.noreply.github.com>
2023-01-10 12:10:45 +0100

    Add files via upload

---
+++ Models/Konekcije/IKonekcija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Konekcije
+{
+    public abstract class IKonekcija<T>
+    {
+        public abstract T Service { get; set; }
+    }
+}
---
+++ Models/Konekcije/KlijentKonekcija.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Konekcije
+{
+    [ExcludeFromCodeCoverage]
+    public class KlijentKonekcija<T> : IKonekcija<T>
+    {
+        NetTcpBinding Binding { get; set; } = null;
+        ChannelFactory<T> Channel { get; set; } = null;
+        EndpointAddress EndpointAddress { get; set; } = null;
+        public KlijentKonekcija(): base() { }
+        public override T Service { get ; set ; }
+
+        public KlijentKonekcija(string uri) : base()
+        {
+            try
+            {
+                Binding = new NetTcpBinding(SecurityMode.None);
+                Channel = new ChannelFactory<T>(Binding);
+                EndpointAddress = new EndpointAddress(uri);
+                Service = Channel.CreateChannel(EndpointAddress);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to create client connection. Exception:{e.Message}");
+            }
+        }
+    }
+}
